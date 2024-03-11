ab1e6ea571ad753e2b899564d3abe1644a43c1a0
yashc1607 <yashc1607@gmail.com>
2024-03-11 21:39:42 +0530

    controller

--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -1,2 +1,12 @@
-import sequelize from "../dbconnect";
-
+import { sequelize } from "../dbconnect.js";
+import { User } from "../Models/User.model.js";
+export const userCreated = sequelize.sync()
+    .then(async () => {
+        // Insert new row using `create()` method
+        await User.create({
+            firstName: 'tommy',
+            lastName: 'Singh'
+        })
+        console.log('Successfully added a new student!')
+    })
+    .catch((error) => console.log('Failed to synchronize with the database:', error))
---
+++ server/Routes/user.routes.js
@@ -0,0 +1,4 @@
+import express from 'express';
+import { userCreated } from '../Controller/userController.js';
+const userRouter = express.Router();
+export default userRouter;
