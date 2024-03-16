23e252cbd2fc08bef4d18215ae5ea39126af6a7b
yashc1607 <yashc1607@gmail.com>
2024-03-16 18:11:30 +0530

    Admin and meeting page

--- server/Routes/user.routes.js
+++ server/Routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addDepartment, addGroup, addRole, addUser, assignGroup, assignKeyword, assignRole, getAllUser, getAssignedGroups, getAssignedKeywords, getAssignedRoles, getDepartments, getGroups, getRoles, getUser, removeAssignedGroup, removeAssignedKeyword, removeAssignedRole, removeDepartment, removeGroup, removeRole, removeUser, updateAssignedRole, updateDepartment, updateGroup, updateRole } from '../Controller/userController.js';
+import { addDepartment, addGroup, addRole, addUser, assignGroup, assignKeyword, assignRole, getAllUser, getAssignedGroups, getAssignedKeywords, getAssignedRoles, getDepartments, getGroups, getRoles, getUser, getUserGroups, removeAssignedGroup, removeAssignedKeyword, removeAssignedRole, removeDepartment, removeGroup, removeRole, removeUser, updateAssignedRole, updateDepartment, updateGroup, updateRole } from '../Controller/userController.js';
 import { addMeeting, addMeetingKeywords, addParticipants } from '../Controller/meetingController.js';
 const userRouter = express.Router();
 
@@ -22,8 +22,10 @@ userRouter.post('/updatedepartment',updateDepartment);
 
 userRouter.post('/addGroup',addGroup);
 userRouter.get('/getGroups',getGroups);
+userRouter.post('/getUserGroups',getUserGroups);
 userRouter.post('/removegroups',removeGroup);
 userRouter.post('/updategroups',updateGroup);
+userRouter.post('/removeAssignedGroup',removeAssignedGroup);
 
 userRouter.post('/assignRole',assignRole);  
 userRouter.get('/getassignedroles',getAssignedRoles);
